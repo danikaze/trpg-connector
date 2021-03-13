92753c9435d16c893f5800067deb9a938f08055b
danikaze <danikaze@gmail.com>
2021-03-13 22:27:41 +0900

    Basic config

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,9 @@
-import * as React from 'react';
-import { render } from 'react-dom';
-import { App } from '@components/app';
+import { msgLog } from '@utils/logging';
 
-const container = document.createElement('div');
-document.body.appendChild(container);
+msgLog('Extension initialized');
 
-const app = <App />;
-render(app, container);
+// const container = document.createElement('div');
+// document.body.appendChild(container);
 
-// just show available constants working
-// can be removed safely
-if (!IS_PRODUCTION) {
-  // tslint:disable-next-line:no-console
-  console.log({
-    PACKAGE_NAME,
-    PACKAGE_VERSION,
-    COMMIT_HASH,
-    COMMIT_HASH_SHORT,
-    IS_PRODUCTION,
-    GLOBAL_EXAMPLE,
-    GLOBAL_SECRET_EXAMPLE,
-  });
-}
+// const app = <App />;
+// render(app, container);
